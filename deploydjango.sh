3fc0df7ad43e2f63ae8b668ca9156eaa258b19db
Yask Srivastava <yask123@gmail.com>
2016-01-11 13:42:06 +0530

    Update deploydjango.sh

--- deploydjango.sh
+++ deploydjango.sh
@@ -3,6 +3,6 @@ sudo apt-get -y install nginx
 sudo apt-get -y install wget
 sudo pip install gunicorn 
 wget https://raw.githubusercontent.com/yask123/nginx-conf/master/nginx.conf
-sudo cp nginx-conf /etc/nginx/nginx.conf
+sudo cp nginx.conf /etc/nginx/nginx.conf
 sudo service nginx start
 gunicorn "$1.wsgi"
