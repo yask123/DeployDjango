8780f32fe823133337e851e90b04c06832778d89
Yask Srivastava <yask123@gmail.com>
2016-01-11 21:54:40 +0530

    Update deploydjango.sh

--- deploydjango.sh
+++ deploydjango.sh
@@ -6,4 +6,5 @@ wget https://raw.githubusercontent.com/yask123/nginx-conf/master/nginx.conf
 sudo cp nginx.conf /etc/nginx/nginx.conf
 sed -i -e 's?{{staticfilepath}}?'$PWD'/'$1'/collectstatic?g' nginx.conf
 sudo service nginx restart
+./manage.py collectstatic
 gunicorn "$1.wsgi"
